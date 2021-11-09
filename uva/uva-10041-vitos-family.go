2639d95cbe18c78b3e74d7c462892954f62fce3b
timwilliam <19308063+timwilliam@users.noreply.github.com>
2021-11-09 13:57:19 +0800

    Completed uva/10041

--- uva/uva-10041-vitos-family.go
+++ uva/uva-10041-vitos-family.go
@@ -1,10 +1,27 @@
+/*
+	Problem		: uva 10041 Vito's Family
+	Author		: timwilliam
+	Compiled	: 2021/11/09
+*/
+
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 )
+
+func intAbs(value int) int {
+	if value < 0 {
+		return -value
+	} else {
+		return value
+	}
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -13,7 +30,41 @@ func main() {
 	scanner.Scan()
 	T, _ := strconv.Atoi(scanner.Text())
 
-	for i := 1; i <= T; i++ {
+	for t := 1; t <= T; t++ {
+		// get number of relatives and street numbers
+		scanner.Scan()
+		inputString := scanner.Text()
+		inputSlice := strings.Split(inputString, " ")
 
+		nRelatives, _ := strconv.Atoi(inputSlice[0])
+		var median, distanceSum int
+
+		// store all the street numbers into streetNumbers slice
+		streetNumbers := make([]int, nRelatives)
+		for i, streetNumber := range inputSlice[1:] {
+			streetNumbers[i], _ = strconv.Atoi(streetNumber)
+		}
+
+		// sort in increasing order
+		sort.Ints(streetNumbers)
+
+		// get the median
+		if nRelatives%2 == 0 {
+			median = nRelatives / 2
+		} else {
+			median = (nRelatives + 1) / 2
+		}
+
+		// calculate the minimal sum of distances
+		distanceSum = 0
+		for i := 0; i < nRelatives; i++ {
+			distance := streetNumbers[median-1] - streetNumbers[i]
+			distance = intAbs(distance)
+
+			distanceSum += distance
+		}
+
+		// print the final result
+		fmt.Println(distanceSum)
 	}
 }
