ecc2107a925a74f7228ea4ae48432cff8c6f55bd
timwilliam <19308063+timwilliam@users.noreply.github.com>
2021-11-22 19:41:02 +0800

    Added sample code for package sort (interface)

---
+++ src/skeleton/package-sort.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (value UserSlice) Len() int {
+	return len(value)
+}
+
+func (value UserSlice) Less(i, j int) bool {
+	return value[i].Age < value[j].Age
+}
+
+func (value UserSlice) Swap(i, j int) {
+	value[i], value[j] = value[j], value[i]
+}
+
+func main() {
+	users := []User{
+		{"Joko", 30},
+		{"Andi", 26},
+		{"Budi", 28},
+		{"Citra", 29},
+	}
+
+	sort.Sort(UserSlice(users))
+	fmt.Println(users)
+}
